ef09c1e4022146953d71462b6d85ec08cc4fe9f3
pwtansg <pwtan.edu@gmail.com>
2025-05-11 15:47:00 +0800

    updated text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          create-react-app is already dead!
+          create-react-app is already dead for so long!
         </a>
       </header>
     </div>
